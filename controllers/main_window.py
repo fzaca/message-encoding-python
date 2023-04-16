9e2144dcaa00d3940ad7249051a2f48aec515ac6
Xukay101 <zaca03zaca@gmail.com>
2023-04-15 23:23:54 -0300

    Add encode and decode system

--- controllers/main_window.py
+++ controllers/main_window.py
@@ -1,3 +1,5 @@
+from utils.encoding_utils import *
+
 from PySide6.QtWidgets import QWidget
 
 from views.main_window_ui import MainWindow
@@ -6,6 +8,45 @@ class MainWindowForm(QWidget, MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.encode_button.clicked.connect(self.encode)
+        self.decode_button.clicked.connect(self.decode)
 
-    def hello(self):
-        print('Hello World')
+    def encode(self):
+        message = self.message_input.toPlainText()
+        if not message:
+            return
+        
+        algorithm = self.algorithm_combobox.currentText()
+        if algorithm == 'Base16':
+            key = base16_encode(message)
+        elif algorithm == 'Rot13':
+            key = rot13_encode(message)
+        elif algorithm == 'Delta':
+            key = delta_encode(message)
+        elif algorithm == 'RLE':
+            key = rle_encode(message)
+        elif algorithm == 'LZW':
+            key = lzw_encode(message)
+
+        self.key_input.setPlainText(key)
+        self.message_input.clear()
+
+    def decode(self):
+        key = self.key_input.toPlainText()
+        if not key:
+            return
+        
+        algorithm = self.algorithm_combobox.currentText()
+        if algorithm == 'Base16':
+            message = base16_decode(key)
+        elif algorithm == 'Rot13':
+            message = rot13_decode(key)
+        elif algorithm == 'Delta':
+            message = delta_decode(key)
+        elif algorithm == 'RLE':
+            message = rle_decode(key)
+        elif algorithm == 'LZW':
+            message = lzw_decode(key)
+
+        self.message_input.setPlainText(message)
+        self.key_input.clear()
